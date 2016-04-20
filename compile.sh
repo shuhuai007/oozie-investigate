31028864541657f9677aac0d10ba80a2b7206932
Jie Zhou <zhoujie338@126.com>
2016-04-20 20:21:53 +0800

    add scp command for shell script

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/env bash
 mvn clean package
 rm workflow/lib/SSHSample*jar
 cp target/*jar workflow/lib
 
+
+echo "scp into oozie server"
+scp -P 2222 -r workflow oozie@127.0.0.1:/home/oozie/work/oozie-investigate
