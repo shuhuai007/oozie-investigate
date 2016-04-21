5937a0535acdd3964383394e7dbe5d169f12075c
Jie Zhou <zhoujie338@126.com>
2016-04-21 08:43:29 +0800

    format the code

--- src/main/java/com/zhoujie/OozieMainBase.java
+++ src/main/java/com/zhoujie/OozieMainBase.java
@@ -4,11 +4,12 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.Tool;
 
+
 public abstract class OozieMainBase extends Configured implements Tool {
     
     protected static final String OOZIE_ACTION_OUTPUT_PROPERTIES =
         "oozie.action.output.properties";    
-    
+
     public final int run(String[] args) throws Exception {
         String oozieConfFile = System.getProperty("oozie.action.conf.xml");
         if(oozieConfFile != null) {
--- workflow/script/hive/test.q
+++ workflow/script/hive/test.q
@@ -1,3 +1,9 @@
 
 
-select count(*) from sda.zhihu_user;
+
+CREATE TABLE IF NOT EXISTS sda.temp
+ROW FORMAT DELIMITED
+        FIELDS TERMINATED BY '1'
+AS
+   select * from sda.zhihu_user where user_agree_num='${USER_AGREE_NUM}';
+
