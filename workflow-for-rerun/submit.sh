3381d59ee9740bfe970a5665246cb96f0961cada
Jie Zhou <zhoujie338@126.com>
2016-04-21 11:13:52 +0800

    refactor the code

---
+++ workflow-for-rerun/submit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+username=oozie
+projectname=ssh-sample
+project_dir=/user/${username}/apps/${projectname}/workflow
+hadoop fs -rmr ${project_dir}
+
+hadoop fs -mkdir -p ${project_dir}
+hadoop fs -put *.sh ${project_dir}
+hadoop fs -put lib ${project_dir}
+hadoop fs -put *.xml ${project_dir}
+hadoop fs -put script ${project_dir}
+
+
+oozie job -config job.properties -run
