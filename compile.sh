bfa6715eaefd84ec1b7b84eff6e76fb3a3b7d6cc
Jie Zhou <zhoujie338@126.com>
2016-04-21 18:52:38 +0800

    refactor the code

--- compile.sh
+++ compile.sh
@@ -2,7 +2,3 @@
 mvn clean package
 rm workflow/lib/SSHSample*jar
 cp target/*jar workflow/lib
-
-
-echo "scp into oozie server"
-scp -P 2222 -r workflow oozie@127.0.0.1:/home/oozie/work/oozie-investigate
--- workflow-for-rerun/submit.sh
+++ workflow-for-rerun/submit.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-
 username=oozie
 projectname=ssh-sample
 project_dir=/user/${username}/apps/${projectname}/workflow
+
 hadoop fs -rmr ${project_dir}
 
 hadoop fs -mkdir -p ${project_dir}
@@ -11,6 +11,5 @@ hadoop fs -put *.sh ${project_dir}
 hadoop fs -put lib ${project_dir}
 hadoop fs -put *.xml ${project_dir}
 hadoop fs -put script ${project_dir}
-
 
 oozie job -config job.properties -run
