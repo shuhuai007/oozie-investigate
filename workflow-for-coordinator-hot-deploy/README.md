885345e31eb91332fccbe9789336e474be1e9956
Jie Zhou <zhoujie338@126.com>
2016-04-25 10:08:15 +0800

    modify config

---
+++ workflow-for-coordinator-hot-deploy/README.md
@@ -0,0 +1,4 @@
+# oozie-investigate
+## workflow for coordinator hot deploy
+
+### 测试在一个coordinator运行过程中，是否可以动态修改workflow.xml，而不需要stop coordinator
---
+++ workflow-for-coordinator-hot-deploy/upload.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "scp into oozie server"
+scp -P 2222 -r * oozie@127.0.0.1:/home/oozie/work/oozie-investigate/coordiantor-hot-deploy
